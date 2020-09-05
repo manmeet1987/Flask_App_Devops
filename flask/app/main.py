cf124f4afda8f5b7d2c93d067a40addcacfec687
Manmeet Singh <manmeet1987@gmail.com>
2020-09-05 14:00:29 +0000

    Modified Hello World

--- flask/app/main.py
+++ flask/app/main.py
@@ -5,7 +5,7 @@ from app import app
 
 @app.route('/')
 def homepage():
-    return "Hello World!!!"
+    return "Hello World Everyone!!!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8000)
